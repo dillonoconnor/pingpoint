78db5229f36d25bd1388a21577ef894674fc4145
Dillon O'Connor <dilong818@gmail.com>
2024-09-17 21:57:05 -0400

    get forms working for each retro cat

--- lib/pingpoint_web/live/retro_live/show.ex
+++ lib/pingpoint_web/live/retro_live/show.ex
@@ -1,15 +1,23 @@
 defmodule PingpointWeb.RetroLive.Show do
   use PingpointWeb, :live_view
 
+  defmodule Comment do
+    defstruct id: Ecto.UUID.generate(), content: nil, author: nil
+  end
+
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
     socket =
       socket
       |> assign(
-        start_doing_form: to_form(%{start_doing: ""}),
-        stop_doing_form: to_form(%{stop_doing: ""}),
-        continue_doing_form: to_form(%{continue_doing: ""})
+        start_doing_form: to_form(%{"start_doing" => ""}),
+        stop_doing_form: to_form(%{"stop_doing" => ""}),
+        continue_doing_form: to_form(%{"continue_doing" => ""}),
+        username: session["username"]
       )
+      |> stream(:start_doing, [])
+      |> stream(:stop_doing, [])
+      |> stream(:continue_doing, [])
 
     {:ok, socket}
   end
@@ -27,31 +35,52 @@ defmodule PingpointWeb.RetroLive.Show do
       <div class="bg-base-300 p-4 rounded-lg flex items-center justify-center text-xl font-semibold">
         Continue Doing
       </div>
-      <div class="p-4 bg-base-200 rounded-lg">
-        <.form for={@start_doing_form}>
-          <label class="input input-bordered flex items-center gap-2">
-            <.icon name="hero-hand-thumb-up" />
-            <input type="text" class="grow" />
-          </label>
-        </.form>
-      </div>
-      <div class="p-4 bg-base-200 rounded-lg">
-        <.form for={@stop_doing_form}>
-          <label class="input input-bordered flex items-center gap-2">
-            <.icon name="hero-hand-thumb-down" />
-            <input type="text" class="grow" />
-          </label>
-        </.form>
-      </div>
-      <div class="p-4 bg-base-200 rounded-lg">
-        <.form for={@continue_doing_form}>
-          <label class="input input-bordered flex items-center gap-2">
-            <.icon name="hero-heart" />
-            <input type="text" class="grow" />
+      <div
+        :for={category <- [:start_doing, :stop_doing, :continue_doing]}
+        class="flex flex-col h-full p-4 bg-base-200 rounded-lg"
+      >
+        <% stringified_category = Atom.to_string(category) %>
+        <div phx-update="stream" id={stringified_category <> "-comments"}>
+          <div :for={{id, comment} <- @streams[category]} class="mt-4 chat chat-start">
+            <div class="chat-header">
+              <%= comment.author %>
+            </div>
+            <div class="chat-bubble">
+              <%= comment.content %>
+            </div>
+          </div>
+        </div>
+        <.form for={assigns[category]} class="mt-auto w-full" phx-submit="add_content">
+          <label for={stringified_category} class="input input-bordered flex items-center gap-2">
+            <.icon name={category_icon(category)} />
+            <input type="hidden" name="author" value={@username} />
+            <input type="hidden" name="category" value={stringified_category} />
+            <input id={stringified_category} type="text" class="grow" name="content" />
           </label>
         </.form>
       </div>
     </div>
     """
+  end
+
+  @impl true
+  def handle_event(
+        "add_content",
+        %{"author" => author, "category" => category, "content" => content},
+        socket
+      ) do
+    {:noreply,
+     stream_insert(socket, String.to_existing_atom(category), %Comment{
+       author: author,
+       content: content
+     })}
+  end
+
+  defp category_icon(category) do
+    case category do
+      :start_doing -> "hero-light-bulb"
+      :stop_doing -> "hero-hand-thumb-down"
+      :continue_doing -> "hero-heart"
+    end
   end
 end
